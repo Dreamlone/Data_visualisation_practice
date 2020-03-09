5baf87666faedaf2f11f98054bfc8c4ca1c12cfc
offdreamlone <mik_sar@mail.ru>
2020-03-09 16:04:34 +0300

    First homework

--- Practice_1/js/index.js
+++ Practice_1/js/index.js
@@ -1,2 +1,17 @@
-const g = document.querySelector('g');
-const circle = document.querySelector('circle');
+function change_function() {
+  var chbox = document.getElementById('one');
+  var box_label = document.getElementById('label');
+  h_smile = document.getElementById("happy");
+  s_smile = document.getElementById("sad");
+  // Если чекбокс находится в состоянии "Sad", то мы переключаемся на "Happy" со всеми преобразованиями
+	if (box_label.innerText === 'Sad') {
+    box_label.innerText = 'Happy';
+    h_smile.style.display = 'block';
+    s_smile.style.display = 'none';
+	}
+	else {
+    box_label.innerText = 'Sad';
+    h_smile.style.display = 'none';
+    s_smile.style.display = 'block';
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Data_visualisation_practice
 Homeworks for Data Visualisation course
+
+First Homework was done - 09.03.2020
